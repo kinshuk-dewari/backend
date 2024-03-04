9f352c6d0760caaf33c806e477ffb98c46706fc2
Kinshuk Dewari <71977053+kinshuk-dewari@users.noreply.github.com>
2024-03-04 20:05:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
-app.get("/all-advisoryComittee",(req,res)=>res.send(here are all the members));
+app.get("/all-advisoryComittee",(req,res)=>res.send("hjdftcghjdfgjdhghjgf"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
